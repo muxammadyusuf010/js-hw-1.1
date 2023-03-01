9d7624c703d5ea3a481908215279113ff8745e40
muxammadyusuf010 <119132083+muxammadyusuf010@users.noreply.github.com>
2023-03-01 03:23:52 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+var name = prompt('Введите свое имя')
+console.log(name);
+
+var age = +prompt('Введите свой возраст')
+console.log(age);
+
+var task1 = +prompt(' 5+6=? ')
+console.log('5+6=11' + '(Ваш ответ ' + task1 + ')');
+
+var task2 = +prompt(' 13-7=? ')
+console.log('13-7=6' + '(Ваш ответ ' + task2 + ')');
+
+var task3 = +prompt(' 21/3=? ')
+console.log('21/3=7' + '(Ваш ответ ' + task3 + ')');
+
+var task4 = +prompt(' 5*6=? ')
+console.log('5*6=30' + '(Ваш ответ ' + task4 + ')');
+
+var task5 = +prompt(' 22%7=? ')
+console.log('22%7=1' + '(Ваш ответ ' + task5 + ')');
+
+alert( 'Зайдите в консоль разработчика, чтобы посмотреть результаты')
+
+
+
+
